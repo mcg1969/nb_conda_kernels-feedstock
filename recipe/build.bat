046a90b996041b7f6f9b6b3e4dd0e81caa53cdbd
Michael C. Grant <mcg@cvxr.com>
2018-05-30 13:42:31 -0500

    Update to 2.1.1

---
+++ recipe/build.bat
@@ -0,0 +1,2 @@
+copy %SRC_DIR%\conda-recipe\post-link.bat Scripts\.nb_conda_kernels-post-link.bat
+copy %SRC_DIR%\conda-recipe\pre-unlink.bat Scripts\.nb_conda_kernels-pre-unlink.bat
---
+++ recipe/build.sh
@@ -0,0 +1,5 @@
+POST_LINK=bin/.nb_conda_kernels-post-link.sh
+PRE_UNLINK=bin/.nb_conda_kernels-pre-unlink.sh
+cp $SRC_DIR/conda-recipe/post-link.sh $POST_LINK
+cp $SRC_DIR/conda-recipe/pre-unlink.sh $PRE_UNLINK
+chmod +x $POST_LINK $PRE_UNLINK
